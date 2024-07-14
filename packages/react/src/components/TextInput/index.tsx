53ff0d267ac3700f4819c81e551e9467d37d3a9d
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-07-14 18:38:12 -0300

    fix: Adjustments on components

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, ElementRef, forwardRef } from 'react'
 import { Prefix, TextInputContainer, Input } from './styles'
 
 export interface TextInputProps extends ComponentProps<typeof Input> {
@@ -7,13 +7,15 @@ export interface TextInputProps extends ComponentProps<typeof Input> {
   disabled?: boolean
 }
 
-export function TextInput({ prefix, ...props }: TextInputProps) {
-  return (
-    <TextInputContainer>
-      {!!prefix && <Prefix>{prefix}</Prefix>}
-      <Input {...props} />
-    </TextInputContainer>
-  )
-}
+export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
+  ({ prefix, ...props }: TextInputProps, ref) => {
+    return (
+      <TextInputContainer>
+        {!!prefix && <Prefix>{prefix}</Prefix>}
+        <Input ref={ref} {...props} />
+      </TextInputContainer>
+    )
+  },
+)
 
 TextInput.displayName = 'TextInput'
